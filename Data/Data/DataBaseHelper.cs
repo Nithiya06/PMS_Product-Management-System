12fe1fe83fde92d50df9508377fe7f50a1d12647
Nithiya C <115150040+Nithiya06@users.noreply.github.com>
2025-02-28 17:09:04 +0530

    Add files via upload

---
+++ Data/Data/DataBaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ApiDBConnection.Data
+{
+    public class DataBaseHelper
+    {
+        private string _connectionString;
+        public DataBaseHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public SqlConnection GetConnection()
+        {
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
